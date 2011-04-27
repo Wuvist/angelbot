411254e1a441d6ee97bd707795325817b42dc7a3
mingyou2007@gmail.com <mingyou2007@gmail.com@300106ab-45b5-2fe3-79f7-829462f1da68>
2011-04-27 09:59:22 +0000

    add statistics

--- trunk/web/AngelWeb/servers/views.py
+++ trunk/web/AngelWeb/servers/views.py
@@ -115,7 +115,6 @@ def rrd_img(request, widget_id):
         if get_time == "1d":
             line += " " + "DEF:donline="+settings.RRD_PATH+widget.rrd.name+".rrd:"+line_diff+":LAST:start="+start+"-"+get_time+":end=start+1d SHIFT:donline:86400 LINE:donline#fbba5c:Yesterday"
         elif get_time == "1w":
-            print get_time
             one_day = " " + "DEF:donline="+settings.RRD_PATH+widget.rrd.name+".rrd:"+line_diff+":LAST:start="+start+"-1d:end=start+1d SHIFT:donline:86400 LINE:donline#fbba5c:Yesterday"
             line += one_day + " " + "DEF:wonline="+settings.RRD_PATH+widget.rrd.name+".rrd:"+line_diff+":LAST:start="+start+"-"+get_time+":end=start+1d SHIFT:wonline:604800 LINE:wonline#b5b5b5:LastWeek"
         return line
@@ -294,6 +293,12 @@ def rrd_show_widget_graph(request, rrd_id, widget_id):
         else:
             check_line_values.append(check_line_value(line, " "))
     
+    if lines == check_lines or len(lines)==1:
+        checkall = "checked"
+        check_line_values[0]=(check_line_value(lines[0], " checked"))
+    else:
+        checkall = ""
+    
     c = RequestContext(request,
         {"rrd":rrd,
         "widget": widget,
@@ -304,5 +309,258 @@ def rrd_show_widget_graph(request, rrd_id, widget_id):
         "lines":lines,
         "check_lines":check_lines,
         "check_line_values": check_line_values,
+        "checkall":checkall,
         })
     return render_to_response('servers/rrd_show_graph.html',c)
+
+def parser(request,widget_id):
+    import matplotlib
+    matplotlib.use('Agg')
+    import numpy as np
+    import time
+    import rrdtool
+    from cStringIO import StringIO
+    from matplotlib import pyplot as plt
+    from subprocess import Popen, PIPE
+    
+    widget = get_object_or_404(Widget,id = widget_id)
+    rrd_name = widget.rrd.name
+    check_lines = request.GET["cl"].replace('[','').replace(']','').replace('u\'','').replace('\'','').replace(' ','').split(',')
+    rrd_lines = rrdtool.fetch(widget.rrd.path(), "-s", str(2), "-e", "s+0", "LAST")[1]
+    if check_lines == ['']:
+        check_lines = rrd_lines
+    start = int(time.mktime(datetime.strptime(request.GET["start"], "%Y-%m-%d^%H:%M:%S").timetuple()))
+    end = int(time.mktime(datetime.strptime(request.GET["end"], "%Y-%m-%d^%H:%M:%S").timetuple()))
+    if start == end:
+        start -= 86400
+    start -= 60
+    cmd = "rrdtool fetch " + widget.rrd.path() + " LAST -s " + str(start) +" -e " + str(end)
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    data_ls = [x.split(" ") for x in stdout.replace(":","").replace("  ","").replace("n","-").replace("N","-").replace("\r","\n").split("\n") ]
+    
+    def parser_time(data_ls,line_number,start_time_number):
+        result = []
+        total = 0
+        x = 0.0000000000001
+        time_def = time.strftime("%Y%m%d%H%M%S",time.localtime(int(data_ls[3][0])))[:start_time_number]
+        last_time = data_ls[-2][0]
+        for i in data_ls:
+            try:
+                times = time.strftime("%Y%m%d%H%M%S",time.localtime(int(i[0])))
+                if i[0] == last_time:
+                    average = total / x
+                    result.append(average)
+                elif times[:start_time_number] == time_def:
+                    total += float(i[line_number])
+                    x += 1
+                else:
+                    time_def = times[:start_time_number]
+                    average = total / x
+                    try:
+                        x = 1.0
+                        total = float(i[line_number])
+                    except:
+                        total = 0
+                        x = 0.0000000000001
+                    result.append(average)
+            except:
+                 pass
+        
+        return result
+    
+    def start(rrd_lines,check_lines,data_ls,start_time_number,p_last_time,time_x):
+        list_all = []
+        times_ls = []
+        for i in range(0,len(rrd_lines)):
+            if rrd_lines[i] in check_lines:
+                list_all.append(parser_time(data_ls, i+1, start_time_number))
+        time_def = time.strftime("%Y%m%d%H%M%S",time.localtime(int(data_ls[3][0])))[:start_time_number]
+        last_time = data_ls[-2][0]
+        for i in data_ls:
+            try:
+                times = time.strftime("%Y%m%d%H%M%S",time.localtime(int(i[0])))
+                if i[0] == last_time:
+                    times_ls.append(eval(p_last_time))
+                elif times[:start_time_number] > time_def:
+                    time_def = times[:start_time_number]
+                    times_ls.append(eval(time_x))
+            except:
+                pass
+        list_all.append(times_ls)
+        return list_all
+    
+    def parser_week(rrd_lines,check_lines,data_ls):
+        def avaerge_week(data_ls,line_number):
+            result = []
+            total = 0
+            j = 10080
+            x = 0.0000000000001
+            data_ls_count = len(data_ls)
+            for i in range(data_ls_count):
+                try:
+                    if i == data_ls_count - 2:
+                        average = total / x
+                        result.append(average)
+                        break
+                    elif i < j:
+                        total += float(data_ls[i][line_number])
+                        x += 1
+                    else:
+                        average = total / x
+                        j += 10080
+                        try:
+                            x = 1.0
+                            total = float(data_ls[i][line_number])
+                        except:
+                            total = 0
+                            x = 0.0000000000001
+                        result.append(average)
+                except:
+                     pass
+            return result
+        
+        def start_week(rrd_lines,check_lines,data_ls,):
+            list_all = []
+            times_ls = []
+            for i in range(0,len(rrd_lines)):
+                if rrd_lines[i] in check_lines:
+                    list_all.append(avaerge_week(data_ls, i+1))
+            j = 10080
+            data_ls_count = len(data_ls)
+            for i in range(data_ls_count):
+                try:
+                    if i == data_ls_count - 2:
+                        times_ls.append(time.strftime("%Y-%m-%d",time.localtime(int(data_ls[i][0])-60)))
+                    elif i == j:
+                        times_ls.append(time.strftime("%Y-%m-%d",time.localtime(int(data_ls[i][0]))))
+                        j += 10080                     
+                except:
+                    pass
+            list_all.append(times_ls)
+            
+            return list_all
+        return start_week(rrd_lines,check_lines,data_ls,)
+    
+    if request.GET["ptime"] =="hour":
+        start_time_number = 10
+        p_last_time = 'time.strftime("%m-%d %H:%M",time.localtime(int(i[0])-60))[:-2]+"00"'
+        time_x = 'time.strftime("%m-%d %H:%M",time.localtime(int(i[0])-3600))'
+        result = start(rrd_lines,check_lines,data_ls,start_time_number,p_last_time,time_x)
+    elif request.GET["ptime"] =="day":
+        start_time_number = 8
+        p_last_time = 'time.strftime("%Y-%m-%d",time.localtime(int(i[0])-60))'
+        time_x = 'time.strftime("%Y-%m-%d",time.localtime(int(i[0])-86400))'
+        result = start(rrd_lines,check_lines,data_ls,start_time_number,p_last_time,time_x)
+    elif request.GET["ptime"] == "week":
+        result = parser_week(rrd_lines,check_lines,data_ls)
+    else:
+        # elif request.GET["ptime"] == "month":
+        start_time_number = 6
+        p_last_time = 'time.strftime("%Y-%m", time.localtime(int(i[0])-60))'
+        time_x = 'time.strftime("%Y-%m",time.localtime(int(i[0])-2419200))'
+        result = start(rrd_lines,check_lines,data_ls,start_time_number,p_last_time,time_x)
+
+    x_values = []
+    if len(result[-1]) >= 18:
+        interval = len(result[-1])/9
+        for i in range(0, len(result[-1]), interval):
+            x_values.append(result[-1][i])
+    else:
+        x_values = result[-1]
+        interval = 1
+
+    fig = plt.figure(figsize=(20,9))
+    for i in range(len(result)-1):
+        plt.plot(result[i])
+    
+    plt.legend(check_lines, loc="upper right",shadow=True)
+    plt.xticks(np.arange(0,len(result[-1]),interval),x_values)
+    plt.title(request.GET["start"]+"_"+request.GET["end"]+"_"+widget.category+"-"+widget.title+"/"+request.GET["ptime"])
+    plt.grid(True)
+    fig.autofmt_xdate()
+    pic_buf = StringIO()
+    plt.savefig(pic_buf,dpi=65)
+    image_data = pic_buf.getvalue()
+    pic_buf.close()
+    plt.close()
+    
+    response = HttpResponse(image_data)
+    response["content-type"] = "image/png"
+    return response
+    
+def show_parse_graph(request,widget_id):
+    import re
+    import time
+    widget = get_object_or_404(Widget, id=widget_id)
+    line = widget.graph_def.replace("{rrd}", widget.rrd.path()).replace('\n','').replace('\r','')
+    lines = re.compile( "LINE:(\w+)" ).findall(line)
+    
+    if request.GET.has_key("start"):
+        start = request.GET["start"]
+    else:
+        start = time.strftime("%Y-%m-%d")
+    if request.GET.has_key("start1"):
+        start1 = request.GET["start1"]
+    else:
+        start1 = time.strftime("%H:%M:%S")
+        
+    if request.GET.has_key("end"):
+        end = request.GET["end"]
+    else:
+        end = time.strftime("%Y-%m-%d")
+    if request.GET.has_key("end1"):
+        end1 = request.GET["end1"]
+    else:
+        end1 = time.strftime("%H:%M:%S")
+        
+    if request.GET.has_key("ptime"):
+        ptime = request.GET["ptime"]
+    else:
+        ptime = "hour"    
+    
+    warn_start = int(time.mktime(datetime.strptime(start + start1, "%Y-%m-%d%H:%M:%S").timetuple()))
+    warn_end = int(time.mktime(datetime.strptime(end + end1, "%Y-%m-%d%H:%M:%S").timetuple()))
+    
+    if warn_start - warn_end > 0:
+        warn = "<div class='errornote'>The end time must be longer than start time or equals !</div>"
+    else:
+        warn = ""
+    check_lines = []
+    for checkbox in lines:
+        if request.GET.has_key(checkbox):
+            check_lines.append(checkbox)
+        
+    check_line_values = []
+    class check_line_value(object):
+        def __init__(self, line, checked):
+            self.line = line
+            self.checked = checked
+    
+    for line in lines:
+        if line in check_lines:
+            check_line_values.append(check_line_value(line, " checked"))
+        else:
+            check_line_values.append(check_line_value(line, " "))
+            
+    if lines == check_lines or len(lines)==1:
+        checkall = "checked"
+        check_line_values[0]=(check_line_value(lines[0], " checked"))
+    else:
+        checkall = ""    
+        
+    c = RequestContext(request,
+        {"widget":widget,
+        "start":start,
+        "start1":start1,
+        "end":end,
+        "end1":end1,
+        "ptime":ptime,
+        "lines":lines,
+        "check_lines":check_lines,
+        "check_line_values": check_line_values,
+        "checkall":checkall,
+        "warn":warn,
+        })
+    return render_to_response('servers/rrd_parse.html',c)
+
