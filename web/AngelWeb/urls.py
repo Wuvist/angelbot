b2ae941daadc5f2e48ba8b80ce16bc62f4916544
mingyou2007 <mingyou2007@gmail.com>
2012-07-06 04:25:15 +0000

    update django from 1.2 to 1.3

--- web/AngelWeb/urls.py
+++ web/AngelWeb/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
     
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^$', 'views.home'),
